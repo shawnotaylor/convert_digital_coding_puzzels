2f17e8b70eab673351c07fc212cbd0ebbec09c66
Shawn Taylor <shawn@palette.com>
2023-03-28 22:00:21 +1100

    soloution found - not general though

--- rickety_bridge/rickety_bridge.js
+++ rickety_bridge/rickety_bridge.js
@@ -1,5 +1,5 @@
 /** 
- * 1. you want to send the fastest group first
+ * 1. send the fastest group first
  * 2. send the slowest people together
  * 3. try send the fastest person alone back
  * 4. repeat until left side is empty
@@ -24,7 +24,7 @@ function findQuickest (array) {
 
 function findQuickestPair (combinations) {
   let index = 0
-  let quickest = combinations[0].quickest
+  let quickest = combinations[0]
   combinations.forEach((element, i) => {
     if (element.total < quickest) {
       index = i
@@ -33,71 +33,70 @@ function findQuickestPair (combinations) {
   return combinations[index]
 }
 
-function travelAlone(to, from, traveler, total_travel_time) {
-  console.log('travelAlone')
-  console.log('left side: ', to)
-  console.log('right side: ', from)
+function travelAlone(to, from, traveler) {
   for (let i = 0; i < from.length; i++) {
-    console.log('in loop: : i=', i, ' traveler=', traveler)
-    if (i === traveler) {
+    if (from[i] === traveler) {
       to.push(traveler);
       from.splice(i, 1);
-      // add time IF we remove an item
-      total_travel_time += traveler
+      return traveler
     }
   }
-  console.log('left side: ', to)
-  console.log('right side: ', from)
 }
 
-function travelPair (from, to, pair, total_travel_time) {
-  console.log('travelPair')
-  console.log('left side: ', from)
-  console.log('right side: ', to)
-  console.log('pair: ', pair)
+function travelPair (from, to, pair) {
   for (let i = 0; i < from.length; i++) {
-    console.log('in loop: : i=', i, ' pair.traveler_one=', pair.traveler_one)
-    if (i === pair.traveler_one) {
-      to.push(to[i]);
-      from.splice(pair.traveler_one, 1);
+    if (from[i] === pair.traveler_one) {
+      to.push(pair.traveler_one);
+      from.splice(i, 1);
+      break
     }
   }
   for (let i = 0; i < from.length; i++) {
-    console.log('in loop: : i=', i, ' pair.traveler_two=', pair.traveler_two)
-    if (i === pair.traveler_two) {
+    if (from[i] === pair.traveler_two) {
       to.push(pair.traveler_two);
       from.splice(i, 1);
+      break
     }
   }
-   // add time IF we remove an item
-   total_travel_time += pair.total
-   console.log('left side: ', from)
-    console.log('right side: ', to)
+  return pair.total
 }
 
 function soloution () {
-  const total_travel_time = 0
+  let total_travel_time = 0
   const times = [1,2,5,10]
   const left_side = times
   const right_side = []
   
-  // console.log('times: ', times)
   let i = 0
-  while (left_side.length > 0 && i < 2) {
-    console.log('loop: ', i)
-    // step 1
-    const combinations = getCombinations(left_side)
-    // console.log('combinations: ', combinations)
+  while (left_side.length > 0) {
+    console.log('total_travel_time: ', total_travel_time)
+    console.log('left side: ', JSON.stringify(left_side))
+    console.log('right side: ', JSON.stringify(right_side))
+    console.log('')
+
+    let combinations = getCombinations(left_side)
     let quickestPair = findQuickestPair (combinations)
-    // console.log('quickest pair: ', quickestPair)
-    travelPair(left_side, right_side, quickestPair, total_travel_time)
-    // console.log('total_travel_time: ', total_travel_time)
-    const fastest = findQuickest(right_side)
-    // console.log('fastest traveller: ', fastest)
-    travelAlone(left_side, right_side, fastest, total_travel_time)
-    // console.log('total_travel_time: ', total_travel_time)
+    total_travel_time += travelPair(left_side, right_side, quickestPair)
+    console.log('total_travel_time: ', total_travel_time)
+    console.log('left side: ', JSON.stringify(left_side))
+    console.log('right side: ', JSON.stringify(right_side))
+    console.log('')
+
+    if (left_side.length > 0) {
+      let fastest = findQuickest(right_side)
+      total_travel_time += travelAlone(left_side, right_side, fastest)
+      console.log('total_travel_time: ', total_travel_time)
+      console.log('left side: ', JSON.stringify(left_side))
+      console.log('right side: ', JSON.stringify(right_side))
+      console.log('')
+    }
+   
     i++
   }
+
+  console.log('all travellers passed')
+  console.log('total time taken = ', total_travel_time)
+
 }
 
 
